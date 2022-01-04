8d831126d16af8dbaf6bdefe4c61cf79e97a4933
Gina Somara <ginasomara15@gmail.com>
2022-01-03 22:20:04 -0500

    deleted test . py

--- test.py
+++
@@ -1,7 +0,0 @@
-import os, binascii
-from backports.pbkdf2 import pbkdf2_hmac
-
-salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
-passwd = "p@$Sw0rD~1".encode("utf8")
-key = pbkdf2_hmac("sha256", passwd, salt, 50000, 32)
-print("Derived key:", binascii.hexlify(key))
